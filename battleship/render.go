6f545cd85d565b311e9f2a6a2bcdd9177cca0075
gjocys <gjocys@Gediminass-MacBook-Pro.local>
2024-02-13 18:34:02 +0200

    add ocean decoration

--- battleship/render.go
+++ battleship/render.go
@@ -3,16 +3,38 @@ package battleship
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
+
+var hHeader = "     " + strings.Join(strings.Split("1,2,3,4,5,6,7,8,9,10", ","), "  ") + "\n"
+var vHeader = map[int]string{
+	0: "A",
+	1: "B",
+	2: "C",
+	3: "D",
+	4: "E",
+	5: "F",
+	6: "G",
+	7: "H",
+	8: "I",
+	9: "J",
+}
+
+var hLine = "  +" + strings.Repeat("-", 32) + "+\n"
 
 func (game *game) renderInfo() {
 	game.buffer.WriteString("Player: " + game.playerName)
 }
 
 func (game *game) renderOcean() {
+	game.buffer.WriteString(hHeader + hLine)
 	for row := range game.ocean.grid {
+
 		for col := range game.ocean.grid[row] {
+			if col == 0 {
+				game.buffer.WriteString(vHeader[row] + " | ")
+			}
 			str := ""
 			cx := game.ocean.grid[row][col]
 			switch cx {
@@ -29,8 +51,14 @@ func (game *game) renderOcean() {
 				str = " ~ "
 			}
 			game.buffer.WriteString(str)
+			if col == 9 {
+				game.buffer.WriteString(" |")
+			}
 		}
 		game.buffer.WriteString("\n")
+		if row == 9 {
+			game.buffer.WriteString(hLine + "\n")
+		}
 	}
 }
 
